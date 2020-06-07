0555e476a38fee92784ea9e891df8bc8c8cd1aea
Hilal Güvenç <hilalguvenc@yahoo.com>
2020-06-07 20:28:37 +0300

    updated

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
-import {products} from "./sample_products";
+import { products } from "./sample_products";
 
-console.log("products", products);
+console.log(products);
 
-
-
-
+var items = $.map(products, function (item) {
+  return (
+    (document.getElementById("imageS").innerHTML = item.image),
+    (document.getElementById("name").innerHTML = item.name),
+    (document.getElementById("price").innerHTML = item.price + " " + "€" + "*"),
+    (document.getElementById("oldPrice").innerHTML =
+      item.oldPrice + " " + "€" + "*"),
+    (document.getElementById("basePrice").innerHTML = item.params.basePrice)
+  );
+});
