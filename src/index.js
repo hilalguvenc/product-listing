1a0ca926b06b580e92be996e40802ead7ddd16c1
Hilal Güvenç <hilalguvenc@yahoo.com>
2020-06-08 17:23:21 +0300

    updated"

--- src/index.js
+++ src/index.js
@@ -1,14 +1,36 @@
 import { products } from "./sample_products";
 
-console.log(products);
-
 var items = $.map(products, function (item) {
-  return (
-    (document.getElementById("imageS").innerHTML = item.image),
-    (document.getElementById("name").innerHTML = item.name),
-    (document.getElementById("price").innerHTML = item.price + " " + "€" + "*"),
-    (document.getElementById("oldPrice").innerHTML =
-      item.oldPrice + " " + "€" + "*"),
-    (document.getElementById("basePrice").innerHTML = item.params.basePrice)
-  );
+  var div = document.createElement("div");
+  div.setAttribute("class", "container");
+  document.body.appendChild(div);
+
+  var image = document.createElement("img");
+  image.setAttribute("src", item.imageS);
+  document.body.appendChild(image);
+
+  var p = document.createElement("p");
+  p.textContent = item.name;
+  div.setAttribute("class", "name");
+  document.body.appendChild(p);
+
+  var p = document.createElement("p");
+  p.textContent = item.price + " " + "€" + "*";
+  p.setAttribute("class", "price");
+  document.body.appendChild(p);
+
+  var p = document.createElement("p");
+  if (item.oldPrice) {
+    p.textContent = item.oldPrice + " " + "€" + "*";
+  } else {
+    p.textContent = "";
+  }
+  p.setAttribute("class", "oldPrice");
+  document.body.appendChild(p);
+
+  var p = document.createElement("p");
+  p.textContent = item.params.basePrice;
+  p.setAttribute("class", "basePrice");
+  document.body.appendChild(p);
 });
+document.write(items);
